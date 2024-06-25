4570cde656ec2e9b830b804df9c1006c58f3dd02
Phits <john@fitz.com>
2024-06-25 13:06:40 -0400

    Event emitters and output

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, computed, input } from '@angular/core';
+import { Component, EventEmitter, Input, Output, computed, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,22 +7,28 @@ import { Component, Input, computed, input } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  // Traditional Inputs
+  // Traditional
+  @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter<string>();
+
+  // New way to do it but not a signal
+  // Exits if using sinal input and simpler code
+  // select = output<string>();
 
   get imagePath() {
     return `assets/users/${this.avatar}`;
   }
 
-  // Signals Opttions
+  onSelectUser() { 
+    this.select.emit(this.id);
+  }
+
+  // Signals Inputs
   // avatar = input.required<string>();
   // name = input.required<string>();
   // imagePath = computed(() => {
   //   return `assets/users/${this.avatar()}`;
   // });
-
-
-
-  onSelectUser() { }
 }
