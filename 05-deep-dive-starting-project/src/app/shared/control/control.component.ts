3b4a9c6fe4ad3186545f9692f0be16e78607a391
Phits <john@fitz.com>
2024-07-08 17:13:08 -0400

    Host elements

--- 05-deep-dive-starting-project/src/app/shared/control/control.component.ts
+++ 05-deep-dive-starting-project/src/app/shared/control/control.component.ts
@@ -1,13 +1,27 @@
-import { Component, input } from '@angular/core';
+import { Component, HostBinding, HostListener, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+    '(click)': 'onClick()'
+  }
 })
 export class ControlComponent {
+  // Old way of binding class. Use host instead.
+  // @HostBinding('class') className = 'control';
+  // @HostListener('click') onClick() {
+  //   console.log('Control clicked');
+  // }
+
   label = input.required<string>();
 
+  // onClick() {
+  //   console.log('Control clicked');
+  // }
 }
