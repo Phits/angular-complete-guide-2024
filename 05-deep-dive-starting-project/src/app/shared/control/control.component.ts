22b2505c821dccffd80f76e3a054ee143a34342a
Phits <john@fitz.com>
2024-07-08 20:55:31 -0400

    Dynamic inline style binding

--- 05-deep-dive-starting-project/src/app/shared/control/control.component.ts
+++ 05-deep-dive-starting-project/src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, HostListener, input, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -20,8 +20,10 @@ export class ControlComponent {
   // }
 
   label = input.required<string>();
+  private el = inject(ElementRef)
 
-  // onClick() {
-  //   console.log('Control clicked');
-  // }
+  onClick() {
+    console.log('Control clicked');
+    console.log(this.el);
+  }
 }
