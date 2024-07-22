29b02f389b237202f15b5c424c8acead3a4767be
Phits <john@fitz.com>
2024-07-22 19:10:32 -0400

    HTTP Interceptors

--- 12-http-requests-starting-project/src/main.ts
+++ 12-http-requests-starting-project/src/main.ts
@@ -1,9 +1,34 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { HttpEventType, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { tap } from 'rxjs';
 
+// Old way was using classes instead of functions for interceptors
+function loggingInterceptor(
+    request: HttpRequest<unknown>,
+    next: HttpHandlerFn) {
+    // CORS Error in Chrome
+    // const req = request.clone({
+    //     headers: request.headers.set('X-DEBUG', 'TESTING')
+    // });
+    console.log('[Outgoing Request]');
+    console.log(request);
+    return next(request).pipe(
+        tap({
+            next: event => {
+                if (event.type === HttpEventType.Response) {
+                    console.log('[Incoming Response]');
+                    console.log(event.status);
+                    console.log(event.body);
+                }
+            }
+        })
+    );
+}
 
 bootstrapApplication(AppComponent, {
-    providers: [provideHttpClient()],
+    providers: [provideHttpClient(
+        withInterceptors([loggingInterceptor])
+    )],
 }).catch((err) => console.error(err));
